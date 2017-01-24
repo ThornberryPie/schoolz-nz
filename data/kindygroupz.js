31f58266d2aef16fe925fd7fe72fbe23b50d78c1
graemet <graeme.thornber@dna.co.nz>
2017-01-24 17:21:00 +1300

    Add kindy data and refs

---
+++ data/kindygroupz.js
@@ -0,0 +1,26 @@
+var kindygroupz = [
+  {
+		"id":"kg1",
+		"name":"BestStart",
+    "website":"www.best-start.org",
+    "phone":"0508 BESTSTART",
+		"email":"info@best-start.org",
+		"image":"kg-beststart.png"
+  },
+  {
+		"id":"kg2",
+		"name":"NKA",
+    "website":"www.nka.org.nz",
+    "phone":"09 435 9099",
+		"email":"admin@nka.org.nz",
+		"image":"kg-nka.png"
+  }/*,
+  {
+		"id":"kg",
+		"name":"",
+    "website":"",
+    "phone":"",
+		"email":"",
+		"image":""
+  }*/
+]
---
+++ data/kindyz.js
@@ -0,0 +1,191 @@
+var kindyz = [
+  /*{
+		"id":"kindy1",
+		"name":"ABC Kaikohe",
+    "address":"15-17 Park Road, Kaikohe, Kaikohe 0405",
+    "phone":"09 401 2806",
+		"group":"BestStart",
+    "lat":"-35.405861",
+    "lng":"173.796528"
+  },
+  {
+		"id":"kindy2",
+		"name":"ABC Kaitaia",
+    "address":"32 Dominion Road, Kaitaia 0410",
+    "phone":"09 408 2653",
+		"group":"BestStart",
+    "lat":"-35.115794",
+    "lng":"173.258857"
+  },*/
+	{
+		"id":"kindy3",
+		"name":"ABC Kamo Central",
+    "address":"2A Lilian Street, Kamo, Whangarei 0112",
+    "phone":"09 435 5454",
+		"group":"BestStart",
+    "lat":"-35.681650",
+    "lng":"174.299460"
+  },
+  {
+		"id":"kindy4",
+		"name":"ABC Kamo Preschool",
+    "address":"Corner Great North Road & Pipiwai Road, RD6, Kamo, Whangarei 0176",
+    "phone":"09 435 3231",
+		"group":"BestStart",
+		"lat":"-35.674932",
+    "lng":"174.297558"
+  },
+  {
+		"id":"kindy5",
+		"name":"ABC Pipiwai Road",
+    "address":"18 Pipiwai Road, RD6, Kamo, Whangarei 0176",
+    "phone":"09 435 3234",
+		"group":"BestStart",
+		"lat":"-35.674930",
+    "lng":"174.296992"
+  },
+	{
+		"id":"kindy6",
+		"name":"ABC Whangarei South",
+    "address":"208 Maunu Rd, Horahora, Whangarei 0110",
+    "phone":"09 438 5030",
+		"group":"BestStart",
+    "lat":"-35.733735",
+    "lng":"174.301647"
+  },
+  {
+		"id":"kindy7",
+		"name":"TopKids Maunu Village",
+    "address":"460 Maunu Road, Maunu Village, Whangarei 0110",
+    "phone":"09 459 6342",
+		"group":"BestStart",
+    "lat":"-35.740033",
+    "lng":"174.281398"
+  },
+  {
+		"id":"kindy8",
+		"name":"TopKids Regent",
+    "address":"2A Deveron Street, Regent, Whangarei 0112",
+    "phone":"09 459 1442",
+		"group":"BestStart",
+    "lat":"-35.713813",
+    "lng":"174.323943"
+  },
+  {
+		"id":"kindy9",
+		"name":"Bream Bay Kindergarten",
+    "address":"Peter Snell Road, Ruakaka 0115",
+    "phone":"09 432 8096",
+		"group":"NKA",
+    "lat":"-35.874945",
+    "lng":"174.456229"
+  },
+  {
+		"id":"kindy10",
+		"name":"Comrie Park Kindergarten",
+    "address":"109 Matarau Road, Matarau 0185",
+    "phone":"09 433 5045",
+		"group":"NKA",
+    "lat":"-35.658869",
+    "lng":"174.222253"
+  },
+  {
+		"id":"kindy11",
+		"name":"Dargaville Kindergarten",
+    "address":"Jaycee Park, Portland St, Dargaville 0310",
+    "phone":"09 439 8875",
+		"group":"NKA",
+    "lat":"-35.938454",
+    "lng":"173.866234"
+  },
+  {
+		"id":"kindy12",
+		"name":"Forest View Kindergarten",
+    "address":"215 Kiripaka Rd, Tikipunga, Whangarei 0112",
+    "phone":"09 437 2965",
+		"group":"NKA",
+    "lat":"-35.684221",
+    "lng":"174.329927"
+  },
+  {
+		"id":"kindy13",
+		"name":"Hikurangi Kindergarten",
+    "address":"5 Park Street, Hikurangi 0114",
+    "phone":"09 433 8535",
+		"group":"NKA",
+    "lat":"-35.602053",
+    "lng":"174.286569"
+  },
+  {
+		"id":"kindy14",
+		"name":"Hillcrest Kindergarten",
+    "address":"Cnr Park & Hillcrest Rd, Kaikohe 0405",
+    "phone":"09 401 0548",
+		"group":"NKA",
+    "lat":"-35.405422",
+    "lng":"173.796028"
+  },
+  {
+		"id":"kindy15",
+		"name":"Hora Hora Kindergarten",
+    "address":"43A First Ave, Avenues, Whangarei 0110",
+    "phone":"09 438 7053",
+		"group":"NKA",
+    "lat":"-35.729175",
+    "lng":"174.313178"
+  },
+  {
+		"id":"kindy16",
+		"name":"Hurupaki Kindergarten",
+    "address":"101 Three Mile Bush Rd Kamo, Whangarei, 0112",
+    "phone":"09 435 3015",
+		"group":"NKA",
+    "lat":"-35.688063",
+    "lng":"174.289378"
+  },
+  {
+		"id":"kindy17",
+		"name":"Kerikeri Kindergarten",
+    "address":"42 Hone Heke Rd, Kerikeri 0230",
+    "phone":"09 407 9639",
+		"group":"NKA",
+    "lat":"-35.228962",
+    "lng":"173.954917"
+  },
+  {
+		"id":"kindy18",
+		"name":"Mairtown Kindergarten",
+    "address":"21 Princes St, Kensington, Whangarei 0112",
+    "phone":"09 437 2742",
+		"group":"NKA",
+    "lat":"-35.706386",
+    "lng":"174.322779"
+  },
+  {
+		"id":"kindy19",
+		"name":"Manaia Kindergarten",
+    "address":"Kiteone Rd, Parua Bay 0174",
+    "phone":"09 436 1262",
+		"group":"NKA",
+    "lat":"-35.770127",
+    "lng":"174.482477"
+  },
+  {
+		"id":"kindy20",
+		"name":"Onerahi Kindergarten",
+    "address":"6 Bayswater Place, Onerahi, Whangarei 0110",
+    "phone":"",
+		"group":"",
+    "lat":"",
+    "lng":""
+  }/*,
+  {
+		"id":"kindy",
+		"name":"",
+    "address":"",
+    "phone":"",
+		"group":"",
+    "lat":"",
+    "lng":""
+  }*/
+]
